cddc0ca4017670eb11d325327fe263d160a1bc27
o080o <o080o@tutanota.com>
2021-12-11 14:46:53 -0600

    added more documentation

--- convert_points.py
+++ convert_points.py
@@ -25,27 +25,31 @@ def plane_projection(center, dx, dy):
     return (center_lon+dx_angle, center_lat+dy_angle)
 
 
-centerpoints=[]
-plots=[]
-features=[]
+centerpoints=[] # a collection of Point features for the plot centers. why not.
+plots=[] # the 'plots: ' json field
+features=[] # the 'features:' json field
+smallest_plot_area=math.inf
 #TODO make filename a parameter
 with open('plots.csv', newline='\n') as csvfile:
     reader=csv.reader(csvfile, delimiter=',')
     #skip the title row
     reader.__next__()
     for row in reader:
+        #name our fields
         fid=int(row[0])
         area=float(row[1])
         lon=float(row[2])
         lat=float(row[3])
-        center=Feature(geometry=Point([lon,lat]))
-        centerpoints.append(center)
+
+        #find the centerpoint
+        center=(lon,lat)
+        centerpoints.append(Feature(geometry=Point([lon,lat])))
+
+        # find the corner points!
         #assuming a flat plane...
         length=math.sqrt(area)
         dx=length/2
         dy=length/2
-
-        center=(lon,lat)
         corners=[
             [
             plane_projection(center, dx, dy),
@@ -56,12 +60,15 @@ with open('plots.csv', newline='\n') as csvfile:
             ]
         ]
 
+        #and add the polygon to our features
         plot=Feature(geometry=Polygon(corners))
+        plots.append(plot)
         # features.append(center)
-        features.append(plot)
+        features.append(plot) #standard convention seems to use the "feature:" json field, output however wants it to be under "plots:" field. adding to "features:" lets standard visualization tools work well
+
+#create and print out the final collection in geojson format
 collection=FeatureCollection(features)
-# collection.centers=centerpoints
-# collection.plots=plots
+collection.plots=plots
 print(collection)
 
 # def binary_search(expected_value, guess1, guess2, max_iter=1000):
